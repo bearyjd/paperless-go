007d45c0d9b2aa05fc4af6796056a01af3fa4606
tower <jd@beary.us>
2026-03-29 10:22:29 -0400

    feat: add DashboardStatistics model and Riverpod provider
    Parses the Paperless-ngx GET api/statistics/ response into a typed
    DashboardStatistics class with a Riverpod async notifier provider.

---
+++ lib/features/dashboard/dashboard_statistics.g.dart
@@ -0,0 +1,31 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dashboard_statistics.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dashboardStatisticsNotifierHash() =>
+    r'e2e98f2a494dc1eba2d5dcee6a982c7e3031d0fa';
+
+/// See also [DashboardStatisticsNotifier].
+@ProviderFor(DashboardStatisticsNotifier)
+final dashboardStatisticsNotifierProvider =
+    AutoDisposeAsyncNotifierProvider<
+      DashboardStatisticsNotifier,
+      DashboardStatistics
+    >.internal(
+      DashboardStatisticsNotifier.new,
+      name: r'dashboardStatisticsNotifierProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$dashboardStatisticsNotifierHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$DashboardStatisticsNotifier =
+    AutoDisposeAsyncNotifier<DashboardStatistics>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
